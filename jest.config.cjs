79a138ce4b3a76a53550b6539bcc12792d32a3a7
Raffaele Carelle <raffaele.carelle@algoritma.it>
2025-09-30 12:29:44 +0200

    Update Jest configuration for ESM support
    - Adjust `ts-jest` preset to `default-esm` and enable ESM handling.
    - Update `tsconfig.json` to use ES2020 `module` instead of CommonJS.

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,24 +1,30 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>'],
-  testMatch: [
-    '<rootDir>/tests/**/*.test.ts'
-  ],
-  transform: {
-    '^.+\\.ts$': 'ts-jest'
-  },
-  collectCoverageFrom: [
-    '*.ts',
-    '!**/*.d.ts',
-    '!**/node_modules/**',
-    '!**/dist/**',
-    '!jest.config.js',
-    '!tests/**',
-    '!index.ts'
-  ],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov', 'html'],
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts']
+    preset: 'ts-jest/presets/default-esm',
+    extensionsToTreatAsEsm: ['.ts'],
+    globals: {
+        'ts-jest': {
+            useESM: true
+        }
+    },
+    testEnvironment: 'node',
+    roots: ['<rootDir>'],
+    testMatch: [
+        '<rootDir>/tests/**/*.test.ts'
+    ],
+    transform: {
+        '^.+\\.ts$': 'ts-jest'
+    },
+    collectCoverageFrom: [
+        '*.ts',
+        '!**/*.d.ts',
+        '!**/node_modules/**',
+        '!**/dist/**',
+        '!jest.config.js',
+        '!tests/**',
+        '!index.ts'
+    ],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'lcov', 'html'],
+    moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+    setupFilesAfterEnv: ['<rootDir>/tests/setup.ts']
 };
